5edd35f30488441fe7ba1fa6f1c2c43fc1259829
bracharedis <bracha@redislabs.com>
2020-08-03 12:09:08 +0300

    end of project

---
+++ print_f_fix_width.py
@@ -0,0 +1,17 @@
+
+
+width = 3
+a_date = '2020-06-11'
+att_date = a_date
+att_time = '10:01:02'
+
+for i in range(12):
+    # print('{0:4}'.format(i), a_date)
+#    print(f"{i:3} : {att_date} {att_time}")
+    print(f"{i:3} : {att_date} {att_time}")
+#
+# >> width = 10
+# >>> precision = 4
+# >>> value = decimal.Decimal('12.34567')
+# >>> f'result: {value:{width}.{precision}}'
+# 'result:      12.35'
